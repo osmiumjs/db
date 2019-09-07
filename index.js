f9bfe97557db3e4cb0d7154e5e62f3ad42e74a3e
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2019-09-08 00:24:35 +0300

    added fk name support

--- index.js
+++ index.js
@@ -11,16 +11,22 @@ Sugar.extend();
 
 function filterName(what) {
 	if (!tools.isString(what)) return what;
-	what = what.trim().split('@')[0].remove('*');
+	what = what.trim().split('=')[0].split('@')[0].remove('*');
 	return what[0] === '>' || what[0] === '<'
 	       ? what.last(what.length - (what[1] === '<') - 1)
 	       : what;
+}
+
+function OsmiumDBError(message, data) {
+	this.message = message;
+	this.data = data;
 }
 
 class DB extends Sequelize {
 	constructor(dbName = 'osmiumapp', user = 'osmiumapp', password = 'masterkey', host = 'localhost', port = 5432, dialect = 'postgres', logging = false, options = {}) {
 		options = Object.assign({dialect, host, logging, port}, tools.isObject(host) ? host : options, options);
 		super(dbName, user, password, options);
+
 		this._options = options;
 		this.Sequelize = Sequelize;
 		this.Op = Op;
@@ -48,85 +54,103 @@ class DB extends Sequelize {
 	}
 
 	/**
-	 * connect tables
+	 * Connect tables (associations)
 	 * @param from - connect what
 	 * @param to - connect to
-	 * @param hasMany
-	 * @param belongsToMany
-	 * @param mountAs
-	 * @param mountAsPk
+	 * @param hasMany - flag for hasMany
+	 * @param belongsToMany - flag for belongsToMany
+	 * @param mountAs - mount as name
+	 * @param fkName - foreignKey name
+	 * @param mountAsPk - modunt ad primary key
 	 */
-	link(from, to, hasMany, belongsToMany, mountAs, mountAsPk = false) {
-		let addMtM = (a, b, through) => {
+	link(from, to, hasMany, belongsToMany, mountAs, fkName, mountAsPk = false) {
+		const addMtM = (a, b, through) => {
 			this.models[a].belongsToMany(this.models[b], through);
 			(this.models[a].mtmThrough = this.models[a].mtmThrough || {})[b] = through;
 		};
-		if (!belongsToMany) {
-			let options = {};
-			if (mountAs) {
-				let allowNull = mountAs.indexOf('!') === -1;
-				mountAs = mountAs.remove('!').trim();
-				options = {
-					foreignKey: {
-						name      : `${mountAs.singularize()}Id`,
-						primaryKey: mountAsPk,
-						allowNull
-					}, as     : mountAs
-				};
-			}
-			this.models[to][hasMany ? 'hasMany' : 'hasOne'](this.models[from], mountAs ? {foreignKey: `${mountAs.singularize()}Id`} : {});
-			this.models[from].belongsTo(this.models[to], options);
-		} else {
-			let through = {through: `${to}@${from}`};
+
+		if (belongsToMany) {
+			const through = {through: `${to}@${from}`};
 			addMtM(from, to, through);
 			addMtM(to, from, through);
 		}
+
+		const options = {};
+
+		if (mountAs) {
+			let allowNull = mountAs.indexOf('!') === -1;
+			mountAs = mountAs.remove('!').trim();
+
+			Object.assign(options, {
+				foreignKey: {
+					name      : fkName ? fkName : `${mountAs.singularize()}Id`,
+					primaryKey: mountAsPk,
+					allowNull
+				},
+				as        : mountAs
+			});
+		}
+
+		if (!this.models[from] || !this.models[to]) {
+			throw new OsmiumDBError('Can\'t find model for link', {to, from, models: Object.keys(this.models), mountAs, fkName});
+		}
+		console.log('>>>', {to, from, models: Object.keys(this.models), mountAs, fkName});
+
+		const fkOptions = mountAs ? {foreignKey: fkName ? fkName : `${mountAs.singularize()}Id`} : fkName ? {foreignKey: fkName} : {};
+		this.models[to][hasMany ? 'hasMany' : 'hasOne'](this.models[from], fkOptions);
+		this.models[from].belongsTo(this.models[to], options);
 	};
 
 	processRelations(schema, parent = false) {
-		if (!tools.isObject(schema)) return;
-		if (parent === '#') return;
+		if (!tools.isObject(schema) || parent === '#') return;
+
 		tools.iterateKeys(schema, (name, out) => {
 			if (parent) {
-				name = name.split('@');
-				let mountTo = (name.length > 1) ? name[1] : false;
+				name = name.split('=');
+				const fkName = name[1] ? name[1] : false;
 				name = name[0];
-				let asPk = name[1] === '*';
-				if (name[0] === '>') {
-					this.link(filterName(name), filterName(parent), true, name[1] === '<', mountTo, asPk);
-				}
-				if (name[0] === '<') {
-					this.link(filterName(parent), filterName(name), true, name[1] === '>', mountTo, asPk);
-				}
+
+				name = name.split('@');
+				const mountTo = (name.length > 1) ? name[1] : false;
+				name = name[0];
+
+				const asPk = name[1] === '*';
+
+				if (name[0] === '>') this.link(filterName(name), filterName(parent), true, name[1] === '<', mountTo, fkName, asPk);
+				if (name[0] === '<') this.link(filterName(parent), filterName(name), true, name[1] === '>', mountTo, fkName, asPk);
 			}
 			this.processRelations(out, name);
 		});
 	};
 
 	registerModel(name, model) {
-		let genMtmFn = (cmd, singularize = false) => function (target, p1 = {}) {
+		const genMtmFn = (cmd, singularize = false) => function (target, p1 = {}) {
 			return this[`${cmd}${this[singularize ? 'singularize' : 'pluralize'](target)}`](p1);
 		};
-		let strcut = {};
+		const strcut = {};
 		let options = {
 			freezeTableName: !!this._options.freezeTableName,
 			timestamps     : !this._options.disableTimestamps
 		};
 		name = filterName(name);
-
-		let useBignum = [];
+		//let useBignum = [];
 
 		tools.iterate(model, (val, key) => {
 			if (key[0] === '#') options = val;
 			if (tools.isObject(val) && val.key) val = val.key;
 			if (!tools.isString(val)) return;
+
 			let notNull = val.indexOf('!') !== -1;
 			val = val.remove('!').split('^');
+
 			let type = this.Sequelize[val[0].toUpperCase()];
+
 			if (!type) return;
 			if (val.length > 1) type = type(...val[1].split(','));
 			type = {type};
+
 			if (notNull) type.allowNull = false;
+
 			if (key[0] === '*') {
 				key = key.substring(1);
 				Object.assign(type, {
@@ -134,7 +158,6 @@ class DB extends Sequelize {
 					autoIncrement: type === this.Sequelize.DataTypes.INTEGER
 				});
 			}
-
 			//if (type.type.key === 'BIGINT') useBignum.push(key);
 
 			strcut[key] = type;
@@ -188,8 +211,10 @@ class DB extends Sequelize {
 
 	cleanParams(what, list) {
 		if (!tools.isObject(what)) return what;
-		let defList = ['id'];
+
+		const defList = ['id'];
 		tools.iterateKeys(list || defList, (name) => { delete what[name]; });
+
 		return what;
 	};
 }
